d11a5d3c5d72a249c9d14ab866ff44f17dd77449
Chandan <caube001@fiu.edu>
2016-10-08 05:10:33 -0400

    used datatables library on volunteerevents view

--- UnitedWay/Controllers/VolunteerEventsController.cs
+++ UnitedWay/Controllers/VolunteerEventsController.cs
@@ -9,11 +9,23 @@ namespace UnitedWay.Controllers
 {
     public class VolunteerEventsController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public VolunteerEventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: VolunteerEvents
         public ActionResult Index()
         {
-            List<VolunteerEvent> volunteerEvents;
-            return View();
+            List<VolunteerEvent> volunteerEvents = _context.VolunteerEvents.ToList();
+            return View(volunteerEvents);
         }
     }
 }
