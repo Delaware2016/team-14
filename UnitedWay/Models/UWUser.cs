75703363cd9a74593b88a10487385a2cce742f77
Chandan <caube001@fiu.edu>
2016-10-07 23:10:09 -0400

    Added fields to user

---
+++ UnitedWay/Models/UWUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitedWay.Models
+{
+    public class UWUser
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string Phone_Number { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+
+    }
+}
