3efb0a5e7893ed935d646d6fe9f763890471e457
Chandan <caube001@fiu.edu>
2016-10-08 06:38:06 -0400

    Added donation routes

---
+++ UnitedWay/Controllers/DonationsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using UnitedWay.Models;
+
+namespace UnitedWay.Controllers
+{
+    public class DonationsController : Controller
+    {
+        // GET: Donations
+        public ActionResult Index()
+        {
+            Donation donation = new Donation()
+                                {
+                                    UserId = System.Web.HttpContext.Current.User.Identity.GetUserId()
+                                };
+            return View(donation);
+        }
+
+        public ActionResult Save(Donation donation)
+        {
+            return View("Index", donation);
+        }
+    }
+}
