ffdaa49b5951abec4b561dfe6ef1c5419aaf640a
Chandan <caube001@fiu.edu>
2016-10-08 04:35:57 -0400

    Added view for event page

---
+++ UnitedWay/Controllers/VolunteerEventsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnitedWay.Models;
+
+namespace UnitedWay.Controllers
+{
+    public class VolunteerEventsController : Controller
+    {
+        // GET: VolunteerEvents
+        public ActionResult Index()
+        {
+            List<VolunteerEvent> volunteerEvents;
+            return View();
+        }
+    }
+}
