e0110f20ba96f24db1c6ffee13de0947c2841580
Chandan <caube001@fiu.edu>
2016-10-07 23:45:24 -0400

    Added uwusers page

---
+++ UnitedWay/Controllers/UWUsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnitedWay.Controllers
+{
+    public class UWUsersController : Controller
+    {
+        // GET: UWUsers
+        public ActionResult Index()
+        {
+            return Content("hi");
+        }
+    }
+}
