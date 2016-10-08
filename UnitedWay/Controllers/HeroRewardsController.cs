076e15fddbf0d88fa8b90474a79a8f48e7cde33f
Chandan <caube001@fiu.edu>
2016-10-08 07:23:04 -0400

    added rewards page

---
+++ UnitedWay/Controllers/HeroRewardsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnitedWay.Controllers
+{
+    public class HeroRewardsController : Controller
+    {
+        // GET: HeroRewards
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
